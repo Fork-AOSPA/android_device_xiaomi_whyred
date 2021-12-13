ca254b01b08ebcb6db3d3a816ea48917e590bec7
anayw2001 <awadhera@berkeley.edu>
2021-12-13 12:40:00 +0000

    Build extphonelib and add to injection overlay
    SystemUI and com.android.phone crash without these

--- device.mk
+++ device.mk
@@ -403,6 +403,10 @@ PRODUCT_PACKAGES += \
     libprotobuf-cpp-full
 
 PRODUCT_PACKAGES += \
+    extphonelib \
+    extphonelib-product \
+    extphonelib.xml \
+    extphonelib_product.xml \
     ims-ext-common \
     ims_ext_common.xml \
     qti-telephony-hidl-wrapper \
