620a212a4bb8fb5ac79416ddbf788186e3eef1ed
Anand Shekhawat <anandsingh215@yahoo.com>
2022-01-31 08:51:56 +0000

    whyred: drop power hal

--- device.mk
+++ device.mk
@@ -355,10 +355,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libqti-perfd-client
 
-# Power
-PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
-
 PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/cgroups_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
     system/core/libprocessgroup/profiles/task_profiles_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
