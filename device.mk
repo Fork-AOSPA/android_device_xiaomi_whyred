3508a8fc9ca9eb7c173de9a4b574f3befc7a6702
Anand Shekhawat <anandsingh215@yahoo.com>
2022-02-01 17:10:43 +0000

    whyred: build android.hidl.allocator@1.0.vendor

--- device.mk
+++ device.mk
@@ -258,7 +258,8 @@ PRODUCT_PACKAGES += \
     libhidltransport \
     libhidltransport.vendor \
     libhwbinder \
-    libhwbinder.vendor
+    libhwbinder.vendor \
+    android.hidl.allocator@1.0.vendor
 
 # IDC
 PRODUCT_COPY_FILES += \
