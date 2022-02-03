96e61158f4e4e2a9d72ff47bf1c7acada12a96a1
Anand Shekhawat <anandsingh215@yahoo.com>
2022-02-03 03:59:28 +0000

    whyred: dont build dummy libqti-perfd-client

--- device.mk
+++ device.mk
@@ -345,10 +345,6 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libstagefrighthw
 
-# Perf
-PRODUCT_PACKAGES += \
-    libqti-perfd-client
-
 PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/cgroups_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
     system/core/libprocessgroup/profiles/task_profiles_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
