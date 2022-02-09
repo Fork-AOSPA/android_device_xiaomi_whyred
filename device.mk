1c7957085e602e29a190038883473d24afedd561
Anand Shekhawat <anandsingh215@yahoo.com>
2022-02-09 07:14:02 +0000

    whyred: Switch to OSS camera hal

--- device.mk
+++ device.mk
@@ -110,19 +110,17 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    Snap
+    camera.sdm660
 
 PRODUCT_PACKAGES += \
-    android.hardware.camera.device@3.5:64 \
-    android.hardware.camera.provider@2.4-impl:32 \
+    android.hardware.camera.common@1.0 \
+    android.hardware.camera.device@3.4 \
+    android.hardware.camera.provider@2.5 \
+    android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    android.hardware.camera.provider@2.5:64 \
-    libdng_sdk.vendor \
-    vendor.qti.hardware.camera.device@1.0:64
-
-# Shim
-PRODUCT_PACKAGES += \
-    libcamera_sdm660_shim
+    vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.device@1.0.vendor \
+    libxml2
 
 # Capability Configstore
 PRODUCT_PACKAGES += \
