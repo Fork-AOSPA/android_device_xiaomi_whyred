ElectroPerf/android_device_asus_sdm660-common

sdm660-common: camera: Mention path for in-tree inclusion of HAL
Change-Id: Ic180a59dfa52d81484471e0b6e630fe545cdaef4
Signed-off-by: Kunmun <kunmun.devroms@gmail.com>

camera/QCamera2/Android.mk
@@ -153,7 +153,7 @@ LOCAL_HEADER_LIBRARIES += libhardware_headers
 
 #HAL 1.0 Include paths
 LOCAL_C_INCLUDES += \
-        hardware/qcom/camera/QCamera2/HAL
+        $(COMMON_PATH)/camera/QCamera2/HAL
 
 ifeq ($(TARGET_COMPILE_WITH_MSM_KERNEL),true)
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include

Fork-AOSPA/android_device_xiaomi_whyred

camera/QCamera2/Android.mk
@@ -153,7 +153,7 @@ LOCAL_HEADER_LIBRARIES += libhardware_headers
 
 #HAL 1.0 Include paths
 LOCAL_C_INCLUDES += \
-        hardware/qcom/camera/QCamera2/HAL
+        $(DEVICE_PATH)/camera/QCamera2/HAL
 
 ifeq ($(TARGET_COMPILE_WITH_MSM_KERNEL),true)
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include