773be2e8c00fcd62d9567057c3ee2a0f08f04bda
Kunmun <kunmun.devroms@gmail.com>
2022-03-31 23:08:09 +0530

    whyred: BoardConfig: Set vendor security patch level
    • As per LA.UM.10.2.1.r1-03400-sdm660.0 board security package,
      from where we will be updating almost all of the vendor blobs.
    
    Change-Id: I5a664bfa0d60f1b621292b092194b0a32e1c5baf

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -66,5 +66,8 @@ TARGET_MOUNT_POINTS_SYMLINKS := true
 # Treble
 BOARD_VNDK_VERSION := current
 
+# Vendor Security patch level
+VENDOR_SECURITY_PATCH := 2022-03-05
+
 # Inherit the proprietary files
 -include vendor/vendor/xiaomi/BoardConfigVendor.mk
