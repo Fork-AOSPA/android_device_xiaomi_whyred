3dacdf9c4274615dfa555f018e34138c415c9899
Anand Shekhawat <anandsingh215@yahoo.com>
2022-03-31 09:21:56 +0530

    whyred: BoardConfig: Import kernel config flags
    • Declare kernel revision
    • Set selinux as permissive
    • Compile kernel with SDLLVM

--- device.mk
+++ device.mk
@@ -10,6 +10,9 @@ $(call inherit-product-if-exists, vendor/xiaomi/whyred/whyred-vendor.mk)
 # Board Platform
 TARGET_BOARD_PLATFORM := sdm660
 
+# Kernel
+TARGET_KERNEL_VERSION := 4.19
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
