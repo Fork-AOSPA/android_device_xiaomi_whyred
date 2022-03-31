c3451fb03dbaa0b2e1b1007972c6f324b05e3770
Kunmun <kunmun.devroms@gmail.com>
2022-03-31 23:26:06 +0530

    whyred: Import common configs from LA.UM.10.2.1.r1-03400-sdm660.0

--- device.mk
+++ device.mk
@@ -7,11 +7,23 @@
 # Inherit proprietary files
 $(call inherit-product-if-exists, vendor/xiaomi/whyred/whyred-vendor.mk)
 
+# AID/fs configs
+PRODUCT_PACKAGES += \
+    fs_config_files
+
 # Board Platform
 TARGET_BOARD_PLATFORM := sdm660
 
+# IPC router config
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
+
 # Kernel
 TARGET_KERNEL_VERSION := 4.19
+
+# Public Libraries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
