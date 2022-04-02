1563c63a18ab1cd4f5fe7734c6a4b6f2f44cfb05
Kunmun <kunmun.devroms@gmail.com>
2022-04-02 09:13:50 +0530

    whyred: liblight: Import from LA.UM.10.2.1.r1-03400-sdm660.0
    • Move to Android.bp as well
    
    Change-Id: Ia75a4aa96b559fcb2ba006bab697f450265060fb

--- device.mk
+++ device.mk
@@ -273,6 +273,11 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
     $(DEVICE_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
 
+# Light
+PRODUCT_PACKAGES += \
+    lights.qcom \
+    android.hardware.lights-service.qti
+
 # Media
 PRODUCT_PACKAGES += \
     android.hardware.media.omx@1.0-impl \
