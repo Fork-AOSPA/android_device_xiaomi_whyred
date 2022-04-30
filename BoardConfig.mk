3ea7b8e38261b75baed763358866b43c3df7070d
Anand Shekhawat <anandsingh215@yahoo.com>
2022-04-30 09:40:29 +0530

    whyred: Initial makefiles for device specific tree

---
+++ BoardConfig.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2022 Paranoid Android
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/xiaomi/whyred
+
+# Inherit the proprietary files
+-include vendor/vendor/xiaomi/BoardConfigVendor.mk
