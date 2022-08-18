02527c308136ad37aa2d5e85fabc7ab45c2c31b2
Anand Shekhawat <anandsingh215@yahoo.com>
2022-08-19 02:44:07 +0530

    whyred: remove hidl light hal

--- device.mk
+++ device.mk
@@ -273,10 +273,6 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
     $(DEVICE_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
 
-# Lights
-PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.xiaomi_sdm660
-
 # Media
 PRODUCT_PACKAGES += \
     android.hardware.media.omx@1.0-impl \
